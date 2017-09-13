1a340b1d5acd85ad6bc393a38ebaf7fa440acbbe
AndresUrregoAngel <ingenieroandresangel@gmail.com>
2017-09-13 16:31:07 -0400

    MapReduce
    Example

---
+++ MapReducer/mod_film.py
@@ -0,0 +1,12 @@
+from mrjob.job import MRJob
+
+class MRCounter(MRJob):
+    def mapper(self,key,line):
+        (userID,movieID,rating,timestamp) = line.split('\t')
+        yield rating,1
+
+    def reducer(self,rating,occurence):
+        yield rating, sum(occurence)
+
+
+MRCounter.run()
