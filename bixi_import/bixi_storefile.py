6e56e65a14bea6d2ba6a6dddc2e11787bb077b65
Andres Urrego <ingenieroandresangel@gmail.com>
2017-09-21 18:07:41 -0400

    Update bixi_storefile.py

--- bixi_import/bixi_storefile.py
+++ bixi_import/bixi_storefile.py
@@ -1,21 +1,43 @@
 import boto3
+import requests
+import json
+import os
+from glob import glob
+import datetime
 from botocore.client import Config
 
-xxxxxx = 'xxxx'
-xxxx= 'xxx'
-BUCKET_NAME = 'x'
+def lambda_handler(event, context):
+    x = 'x'
+    x = 'x'
+    BUCKET_NAME = 'x'
 
-s3 = boto3.resource(
-    's3',
-    aws_access_key_id=xxxx,
-    aws_secret_access_key=xxxxxxx,
-    config=Config(signature_version='s3v4')
-)
+    s3 = boto3.resource(
+        's3',
+        aws_access_key_id=x,
+        aws_secret_access_key=x,
+        config=Config(signature_version='s3v4')
+    )
 
+    file = 'website-here'
+    result = requests.get(file)
+    data = result.json()
+    now = datetime.datetime.now()
+    hour = now.hour
+    day = now.day
+    month = now.month
+    mins = now.minute
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+    a=glob("/tmp/*.json")
+    for file in a:
+        os.remove(file)
 
-FILE_NAME = 'file'
-s3_client = boto3.client('s3')
-s3_client.upload_file(FILE_NAME, BUCKET_NAME, FILE_NAME)
+    with open('/tmp/Bixi_%s_%s_%s-%s.json' % (month, day, hour, mins), 'w') as outfile:
+        json.dump(data, outfile)
+
+    filename = 'Bixi_%s_%s_%s-%s.json' % (month, day, hour, mins)
+    datafile = open('/tmp/Bixi_%s_%s_%s-%s.json' % (month, day, hour, mins),'rb')
+
+    s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=datafile)
+    return print("process completed succesfully")
+
+#lambda_handler('n', 'n')
