a97e691648ff33c8ad4dc70522e1329ace094cf2
Andres Urrego <ingenieroandresangel@gmail.com>
2018-01-13 17:16:26 -0800

    New course

--- Quick Course/Exercices/notes_scripts.py
+++ Quick Course/Exercices/notes_scripts.py
@@ -6,30 +6,22 @@ with open('E:\\Sources\\student.txt') as file:
    next(file)
    for item in file:
         line = item.split(',')
-        f = (line[3]+','+line[4]+','+line[5]+','+line[6]+','+line[7]+','+line[8]+','+line[9].replace('\n',''))
+        f = (line[3].strip()+','+line[4].strip()+','+line[5].strip()+','+line[6]+','+line[7]+','+line[8]+','+line[9].replace('\n',''))
         grades.append(f)
 
 
 reports = {}
 number_report = range(11)
+results = []
 
 
 for report in number_report:
     reports[report] = grades[report]
 
 for k,v in reports.items():
-        print(k,v)
-'''
-
-with open('E:\\Sources\\student.txt') as file:
-    next(file,None)
-    for i in file:
-        item = i.split(',')
-        for report in number_report:
-            reports[report] = item
-
-
-    for k,v in reports.items():
-        print(v)
-
-'''
+       while (k == 0 ):
+           for i in v:
+               grade = i.strip().split('\n')
+               if (grade > 3):
+                print(grade)
+           k+=1
