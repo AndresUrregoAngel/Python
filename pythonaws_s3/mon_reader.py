d9aff00c1cd5315e7af38f87f3d4340b36a39e51
Andres Urrego <ingenieroandresangel@gmail.com>
2018-03-20 17:02:42 -0400

    navigating buckets

--- pythonaws_s3/mon_reader.py
+++ pythonaws_s3/mon_reader.py
@@ -1,24 +1,34 @@
 import boto3
 import datetime
+import datetime
+ts = datetime.datetime.now() #.timestamp()
+
 
 # Create an S3 client
 s3 = boto3.client('s3')  #Client
 s3resource = boto3.resource('s3') #Ressources
 
-"""
+
 # Call S3 to list current buckets
 response = s3.list_buckets()
 # Get a list of all bucket names from the response
-for bucket in s3resource.buckets.all():
-    print(bucket.name)
+#for bucket in s3resource.buckets.all():
+ #   print(bucket.name)
+
+buckets = []
 
 for bucket in response['Buckets']:
-    print(bucket['Name'])
+    buckets.append(bucket)
 
-"""
+for name in buckets:
+    for i in s3resource.Bucket(name).objects.all():
+        print("bucket {} objects {} ".format(name,i))
+
+
 # Load a file
 
-filename = '',
+filename = "test_"+str(ts.now())
 filepath = 'C:\\Users\\a_urrego\\Documents\\DW Project\\datasources\\test.txt'
 bucket_name = 'poc-developers'
-s3.upload_file(filename, bucket_name, filepath)
+s3.upload_file(filepath, bucket_name, filename)
+
