1e4d8299b12d6b1ea6dc8c9c9979971559556419
Andres Urrego <ingenieroandresangel@gmail.com>
2018-03-20 16:30:20 -0400

    Update mon_reader.py

--- pythonaws_s3/mon_reader.py
+++ pythonaws_s3/mon_reader.py
@@ -1,5 +1,24 @@
-import boto
+import boto3
+import datetime
 
-conn = boto.connect_s3('xxx' ,'xxx')
-bucket = conn.create_bucket('file')
+# Create an S3 client
+s3 = boto3.client('s3')  #Client
+s3resource = boto3.resource('s3') #Ressources
 
+"""
+# Call S3 to list current buckets
+response = s3.list_buckets()
+# Get a list of all bucket names from the response
+for bucket in s3resource.buckets.all():
+    print(bucket.name)
+
+for bucket in response['Buckets']:
+    print(bucket['Name'])
+
+"""
+# Load a file
+
+filename = '',
+filepath = 'C:\\Users\\a_urrego\\Documents\\DW Project\\datasources\\test.txt'
+bucket_name = 'poc-developers'
+s3.upload_file(filename, bucket_name, filepath)
