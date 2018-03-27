a7cafb1122166bec9f24d9bc853cbf3c35051178
Andres Urrego <ingenieroandresangel@gmail.com>
2018-03-26 19:13:57 -0700

    dictionaries

--- various/salaries.py
+++ various/salaries.py
@@ -1,3 +1,4 @@
+"""
 file = open("E:\\Sources\\Salaries\\rawdata.csv","r")
 values = []
 for i in file:
@@ -9,3 +10,50 @@ for i in values:
     sum += i
 
 print(sum)
+
+
+"""
+
+files = {
+    'Input.txt': 'Randy',
+    'Code.py': 'Stan',
+    'Output.txt': 'Randy'
+}
+
+
+def group_by_owners(files):
+    dic = {}
+    for k,v in files.items():
+        if( v in dic.keys()):
+            key = v
+            dic.setdefault(key, []).append(k)
+        else:
+            key = v
+            dic.setdefault(key, []).append(k)
+
+
+
+    return dic
+
+print(group_by_owners(files))
+
+
+
+
+
+"""
+def is_palindrome(word):
+    answer = ''
+    if (word.lower()  == word.lower()[::-1]):
+        #print('True')
+        answer = 'True'
+    else:
+        #print('False')
+        answer = 'False'
+    return answer
+
+
+
+
+print(is_palindrome('Deleveled'))
+"""
