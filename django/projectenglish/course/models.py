e2208d2f05956bccfd13edea2b6ebd76252c6c74
Andres Urrego <ingenieroandresangel@gmail.com>
2018-03-31 17:38:04 -0700

    New update for login

--- django/projectenglish/course/models.py
+++ django/projectenglish/course/models.py
@@ -1,18 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class PortalUsers(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='course_users')
+    profile = models.CharField(max_length=30)
+
 
 class subject(models.Model):
     subj_name  = models.CharField(max_length=30)
 
 
+class subject_category(models.Model):
+    subject    = models.ForeignKey(subject, on_delete=models.CASCADE)
+    category_name  = models.CharField(max_length=30)
+
+
 class questions(models.Model):
-    question  = models.ForeignKey(subject, default= int, on_delete=models.CASCADE)
+    category  = models.ForeignKey(subject_category, on_delete=models.CASCADE)
     question_desc = models.CharField(max_length=1000)
 
 
 class answer(models.Model):
-    option_sub =models.ForeignKey(subject,default= int, on_delete=models.CASCADE)
-    option_que = models.ForeignKey(questions, default= int, on_delete=models.CASCADE)
-    option_desc = models.CharField(max_length=500)
+    subject = models.ForeignKey(subject, on_delete=models.CASCADE)
+    category  = 	models.ForeignKey(subject_category, on_delete=models.CASCADE)
+    questions = models.ForeignKey(questions, on_delete=models.CASCADE)
+    answer_desc = models.CharField(max_length=500)
     correct = models.IntegerField()
